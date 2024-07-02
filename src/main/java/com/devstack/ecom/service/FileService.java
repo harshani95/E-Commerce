b0752ca3ea8ec17f5fea7380eb4dd1d005d7f4c6
harshani <ruwanthikapemarathna@gmail.com>
2024-07-02 08:17:09 +0530

    Implementation of Product Image

---
+++ src/main/java/com/devstack/ecom/service/FileService.java
@@ -0,0 +1,10 @@
+package com.devstack.ecom.service;
+
+import com.devstack.ecom.util.CommonFileSavedBinaryDataDTO;
+import org.springframework.web.multipart.MultipartFile;
+
+public interface FileService {
+    public CommonFileSavedBinaryDataDTO createResource(MultipartFile file, String directory, String bucket);
+    public void deleteResource(String bucket,String directory, String fileName);
+    public byte[] downloadFile(String bucket, String fileName);
+}
