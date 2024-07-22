6401135e0d72cb948b6530c9f5b55ea38ad62433
harshani <ruwanthikapemarathna@gmail.com>
2024-07-22 23:05:47 +0530

    JWT authentication part I

---
+++ src/main/java/com/devstack/ecom/service/impl/ApplicationUserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.devstack.ecom.service.impl;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ApplicationUserServiceImpl implements UserDetailsService {
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return null;
+    }
+}
